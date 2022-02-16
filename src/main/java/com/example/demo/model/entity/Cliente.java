95a844531bfda250c7ce5c81d4a1c0727c2c4147
EdsonSantana93 <edson.fs193@gmail.com>
2022-02-15 22:55:12 -0300

    Adicionado Construtores, Gettes e Setters

--- src/main/java/com/example/demo/model/entity/Cliente.java
+++ src/main/java/com/example/demo/model/entity/Cliente.java
@@ -23,4 +23,49 @@ public class Cliente {
 
 	@Column(name = "data_cadastro")
 	private LocalDate dataCadastro;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, LocalDate dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
 }
