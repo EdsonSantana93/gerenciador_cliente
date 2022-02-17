38ef42f7bceeff552f8386a7e69ec2abc653bcf9
EdsonSantana93 <edson.fs193@gmail.com>
2022-02-16 21:50:47 -0300

    Iniciando a API de Cliente/ Salvar Cliente

--- src/main/java/com/example/demo/model/entity/Cliente.java
+++ src/main/java/com/example/demo/model/entity/Cliente.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class Cliente {
 
@@ -22,18 +24,20 @@ public class Cliente {
 	private String cpf;
 
 	@Column(name = "data_cadastro")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataCadastro;
 
 	public Cliente() {
 		super();
+		this.dataCadastro = LocalDate.now();
 	}
 
-	public Cliente(Integer id, String nome, String cpf, LocalDate dataCadastro) {
+	public Cliente(Integer id, String nome, String cpf) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
-		this.dataCadastro = dataCadastro;
+		this.dataCadastro = LocalDate.now();
 	}
 
 	public Integer getId() {
@@ -64,8 +68,8 @@ public class Cliente {
 		return dataCadastro;
 	}
 
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
+	public void setDataCadastro() {
+		this.dataCadastro = LocalDate.now();
 	}
 
 }
